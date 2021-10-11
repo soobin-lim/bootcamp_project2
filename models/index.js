d9a8e4890153fe79a5f5fbed2a6f79c983619d88
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-10-11 00:34:22 -0500

    4

--- models/index.js
+++ models/index.js
@@ -1,32 +1,17 @@
-const fs = require('fs');     // be carefaul
-const path = require('path');    // be carefaul
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);    // be carefaul
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-
-// __dirname = /Users/bootcamp/tdm-mxc-fsf-pt-06-2021-u-c/bootcamp_project2/models
 const db = {};
 
 let sequelize;
-if (process.env.JAWSDB_URL || config.use_env_variable) {
-  // for Heroku
-  console.log('config.use_env_variable',config.use_env_variable)
-  console.log('process.env.JAWSDB_URL', process.env.JAWSDB_URL);
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {});
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  if (config.use_env_variable) {
-    //for Heroku
-    console.log('config.use_env_variable',config.use_env_variable)
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  } else {
-    console.log('config.use_env_variable',config.use_env_variable)
-    sequelize = new Sequelize(config.database, config.username, config.password, config);   //Dialect needs to be explicitly supplied as of v4.0.0
-    // using .env's information..
-    // console.log('option2', config.database, config.username, config.password, config);
-  }
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 // DB setup
 // const db = {};
 
@@ -40,8 +25,6 @@ if (process.env.JAWSDB_URL || config.use_env_variable) {
 //   const config = require(__dirname + '/../config/config.json')[env];
 //   sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
-
-
 
 const model2 = require('./materialmaster/kiaandsapmaterial')(sequelize, Sequelize);
 
