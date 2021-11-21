a73283dcb32f4a5f1e5a669602e3082f88d8c14c
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-11-21 11:54:08 -0600

    force:true

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const passport = require("passport-local")
 
 const db = require("./models");
 
-db.sequelize.sync({ force: false })
+db.sequelize.sync({ force: true })
   .then(() => {
     console.log(`Database & tables created!`)
   }).catch(err => console.log('sequelize sync force error  :  ' + err))
